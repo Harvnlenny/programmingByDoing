f886058fdf5bb7dbfd3561319f31570d10827192
Aaron Harvey <harvnlenny@gmail.com>
2016-02-29 21:24:01 -0500

    added Variables

---
+++ variables/Variables.java
@@ -0,0 +1,17 @@
+public class Variables 
+{
+  public static void main( String[] args)
+  {
+    String course;
+    int room;
+    double e;
+
+    course = "Computer Science";
+    room = 113;
+    e = 2.71828;
+
+    System.out.println( "This is room # " + room + "." );
+    System.out.println( "e is close to " + e + "." );
+    System.out.println( "I am learning a bit about " + course + "." );
+  }
+}
