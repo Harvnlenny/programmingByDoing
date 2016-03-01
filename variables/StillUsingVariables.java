109530a45f85e239577ec28c74815d46899833e2
Aaron Harvey <harvnlenny@gmail.com>
2016-02-29 21:27:56 -0500

    added StillUsingVariables

---
+++ variables/StillUsingVariables.java
@@ -0,0 +1,13 @@
+public class StillUsingVariables 
+{
+  public static void main( String[] args)
+  {
+    String name;
+    int year;
+
+    name = "Aaron Harvey";
+    year = 1994;
+
+    System.out.println("My name is " + name + " and I graduated high school in " + year + ".");
+  }
+}
