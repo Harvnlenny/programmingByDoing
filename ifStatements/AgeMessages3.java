3987d83d171bf40f014265ba10b085e317952cfc
Aaron Harvey <harvnlenny@gmail.com>
2016-03-12 08:56:28 -0500

    Added AgeMessages3

---
+++ ifStatements/AgeMessages3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AgeMessages3
+{
+  public static void main( String[] args)
+  {
+    Scanner keyboard = new Scanner(System.in);
+    String name;
+    int age;
+
+    System.out.print( "Your name: ");
+    name = keyboard.next();
+
+    System.out.print( "Your age: ");
+    age = keyboard.nextInt();
+
+    if ( age < 16 )
+    {
+      System.out.println( "You can't drive, " + name + ".");
+    }
+    if ( age < 18 && age > 16)
+    {
+      System.out.println( "You can drive but not vote, " + name + ".");
+    }
+    if ( age > 17 && age < 25 )
+    {
+      System.out.println( "You can vote but not rent a car, " + name + ".");
+    }
+    if ( age >= 25 )
+    {
+      System.out.println( "You can do pretty much anything, " + name + ".");
+    }
+  }
+}
