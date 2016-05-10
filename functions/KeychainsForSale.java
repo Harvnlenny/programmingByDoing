dacee5b0b63e73088617efc064bddb3c6bef8fff
Harvnlenny <harvnlenny@gmail.com>
2016-05-09 21:56:31 -0400

    added KeychainsForSale

---
+++ functions/KeychainsForSale.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class KeychainsForSale{
+  public static void main (String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    int choice;
+
+    System.out.println("Ye Olde Keychain Shoppe\n");
+    do{
+      System.out.println("1. Add Keychains to Order");
+      System.out.println("2. Remove Keychains from Order");
+      System.out.println("3. View Current Order");
+      System.out.println("4. Checkout\n");
+      System.out.print("Please enter your choice: ");
+      choice = keyboard.nextInt();
+        if (choice == 1){
+          add_keychains();
+        }
+        else if (choice == 2){
+          remove_keychains();
+        }
+        else if (choice == 3){
+          view_order();
+        }
+        else if (choice == 4){
+          checkout();
+        }
+    } while (choice != 4);
+  }
+  public static void add_keychains(){
+    System.out.println("\nADD KEYCHAINS\n");
+    return;
+  }
+  public static void remove_keychains(){
+    System.out.println("\nREMOVE KEYCHAINS\n");
+    return;
+  }
+  public static void view_order(){
+    System.out.println("\nVIEW ORDER\n");
+    return;
+  }
+  public static void checkout(){
+    System.out.println("\nCHECKOUT");
+    return;
+  }
+}
