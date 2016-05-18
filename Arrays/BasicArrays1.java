ad6f85767685b4647b94621bd620fb1cfed7691c
Harvnlenny <harvnlenny@gmail.com>
2016-05-17 21:51:46 -0400

    add BasicArrays1

---
+++ Arrays/BasicArrays1.java
@@ -0,0 +1,12 @@
+public class BasicArrays1
+{
+  public static void main( String[] args )
+  {
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.length; i++)
+    {
+        arr [i] = -113;
+        System.out.println("Slot " + i + " contains a " + arr[i]);
+    }
+  }
+}
