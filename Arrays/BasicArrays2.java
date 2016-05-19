95d0ad7e44642c3e4aed226f3a992ab2da898d9b
Harvnlenny <harvnlenny@gmail.com>
2016-05-18 23:05:46 -0400

    added BasicArrays2

---
+++ Arrays/BasicArrays2.java
@@ -0,0 +1,13 @@
+public class BasicArrays2
+{
+  public static void main( String[] args )
+  {
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.length; i++)
+    {
+        arr [i] = 1 + (int)(Math.random()*100);
+        System.out.println("Slot " + i + " contains a " + arr[i]);
+    }
+
+  }
+}
